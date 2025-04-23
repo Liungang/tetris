fe25e2688efc880a89fa5f5fa87f34f20d898d06
Nikou <ninggangliu@agiforge.com>
2025-04-23 17:07:13 +0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  // 设置基础路径为仓库名称，适配GitHub Pages部署
+  base: '/tetris/',
   plugins: [vue()],
   server: {
     port: 3000
